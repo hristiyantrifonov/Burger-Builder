fb91212b427f5f410ddf6ab14c877d45d5a42a3b
Hris <hristiyantrifonov@gmail.com>
2018-09-10 22:35:17 +0100

    Basic Build Controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,26 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    //Map the object into array of ingredients
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => { //igKey is salad, bacon etc. // i is 1,2,3 etc.
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}/>;
+            })
+        }) // Array of array up to here
+        .reduce((arr,element) => {
+            return arr.concat(element)
+        }, []); // either array or the elements
+
+   if (transformedIngredients.length === 0){
+       transformedIngredients = <p>Please start adding ingredients!</p>
+   }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
