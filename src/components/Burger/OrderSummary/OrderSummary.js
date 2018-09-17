3790f2a16f82a39f71039e58cf92f9e5ca35d663
Hris <hristiyantrifonov@gmail.com>
2018-09-18 00:04:53 +0100

    Burger Builder Main Application Finished

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
     //A modal holding the order summary
-    
-    //Transform into array of keys (salad, bacon etc.)
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return <li key={igKey}>
-                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                    </li>
-        });
+    //This could be a functional component, doesn't have to be a class
 
-    return (
-      <Aux>
-          <h3>Your Order</h3>
-          <p>Delicious burger with following ingredients:</p>
-          <ul>
-              {ingredientSummary}
-          </ul>
-          <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-          <p>Continue to checkout?</p>
-          <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-          <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-      </Aux>
-    );
-};
+    componentWillUpdate(){
+        console.log('[OrderSummary] WillUpdate')
+    }
 
-export default orderSummary;
+    render(){
+
+        //Transform into array of keys (salad, bacon etc.)
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>
+            });
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>Delicious burger with following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+}
+
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 import classes from './Modal.css';
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
+class Modal extends Component{
 
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
+    //In this case this method is straight forward but in other cases
+    //where we do more stuff here, we should check for changes in the
+    //other props and not just the 'show' prop
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show;
+    }
 
-        <div className={classes.Modal}
-             style={{
-                 transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-                 opacity: props.show ? '1' : '0'
-             }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+    componentWillUpdate(){
+        console.log('[Modal] WillUpdate');
+    }
 
-export default modal;
+    render(){
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal}
+                     style={{
+                         transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+                         opacity: this.props.show ? '1' : '0'
+                     }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+}
+
+export default Modal;
