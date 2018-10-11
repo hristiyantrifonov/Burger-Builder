d600b5a0c003e8f69492297ade40ed1cc2904ac9
Hris <hristiyantrifonov@gmail.com>
2018-10-11 10:59:14 +0100

    Form submission, handling input and showing errors + Wrap up

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -16,7 +16,12 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -24,7 +29,12 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -32,7 +42,14 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'ZIP Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 6
+                },
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: 'input',
@@ -40,7 +57,12 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: 'input',
@@ -48,7 +70,12 @@ class ContactData extends Component{
                     type: 'email',
                     placeholder: 'Your E-Mail'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -58,20 +85,28 @@ class ContactData extends Component{
                         {value: 'cheapest', displayValue: 'Cheapest'}
                         ]
                 },
-                value: ''
+                value: 'fastest',
+                validation: {},
+                valid: true
             }
         },
+        formIsValid: false,
         loading: false
     }
 
     orderHandler = (event) => {
         event.preventDefault();
         this.setState({loading: true});
+        let formData = {};
+        for(let formElementIdentifier in this.state.orderForm){
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+
+        }
 
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,   //!!! If this goes into real app - this price should be calculated in the server to avoid manipulation
-
+            orderData: formData
         }
 
         //Creates the orders node(table) in Firebase
@@ -87,6 +122,66 @@ class ContactData extends Component{
             });
     }
 
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if(!rules) {
+            return true;
+        }
+
+        // Fixing common validation gotcha
+        // Now all rules need to resolve to true not just one
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
+    //Two-way binding so that when we type into the input elements the state updates
+    inputChangedHandler = (event, inputIdentifier) => {
+        //console.log(event.target.value, inputIdentifier);
+
+        // doing just ...this.state.orderForm would not deeply copy the object
+        // this is the case because it is not one level deep, it has multiple
+        // nested elements and this means that the pointers to them would be copied
+        // and we will still change the state mutably
+
+        // this is clone of the original object
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+
+        // then we take care of the nested object
+        // now we can safely change data in the value
+        // if we were to change something in elementConfig - we need
+        // another clone as it is still not copied.
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        }
+
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+        updatedFormElement.touched = true;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement);
+
+        let formValidity = true;
+        for(let inputIdentifier in updatedOrderForm){
+            formValidity = updatedOrderForm[inputIdentifier].valid && formValidity;
+        }
+
+        this.setState({orderForm: updatedOrderForm, formIsValid: formValidity});
+    }
+
     render () {
 
         const formElementsArray = [];
@@ -99,15 +194,19 @@ class ContactData extends Component{
 
 
         let form =(
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
                     <Input
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value}/>
+                        value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                <Button btnType="Success" disabled={!this.state.formIsValid} clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
 
