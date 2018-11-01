6b66b0f6639b7d8eb0c6adba125160e4e2c6952c
Hris <hristiyantrifonov@gmail.com>
2018-11-01 01:30:05 +0000

    Add Firebase Authentication - Signing up and Signing in requests

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,4 @@
 // Central file that will combine all exports
 export { addIngredient, removeIngredient, initIngredients } from './burgerBuilder';
 export { purchaseBurger, purchaseInit, fetchOrders } from './order';
+export { auth } from './auth';
